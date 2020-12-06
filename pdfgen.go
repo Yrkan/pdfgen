feb287593c94881af9e078807233d4fab5ff4999
Thami Memel <memelthami@gmail.com>
2020-12-06 11:05:16 +0100

    add Write to io writer

--- pdfgen.go
+++ pdfgen.go
@@ -1,7 +1,10 @@
 package pdfgen
 
 import (
+	"io"
+	"io/ioutil"
 	"log"
+	"os"
 	"os/exec"
 	"path"
 	"reflect"
@@ -9,31 +12,43 @@ import (
 	"github.com/nguyenthenguyen/docx"
 )
 
-// Save tt
-func Save(template string, data interface{}) {
+// Write to IO Writer
+func Write(template string, data interface{}, w io.Writer) {
+	Save(template, data, "./temp.pdf")
+
+    pdf, err := ioutil.ReadFile("./temp.pdf")
+	if (err != nil) {
+		log.Fatal(err)
+	}     
+	w.Write(pdf)
+	os.Remove("./temp.pdf")
+}
+
+// Save to outpath
+func Save(template string, data interface{}, outpath string) {
 	processDocument(template, data)
 	arg0 := "lowriter"
 	arg1 := "--invisible" //This command is optional, it will help to disable the splash screen of LibreOffice.
 	arg2 := "--convert-to"
 	arg3 := "pdf:writer_pdf_Export"
 	arg4 := "--outdir"
-	arg5 := path.Dir(template)
-	path := path.Dir(template) + "/temp.docx"
-	_, err := exec.Command(arg0,arg1,arg2,arg3, arg4, arg5 ,path).Output()
+	arg5 := path.Dir(outpath)
+	dpath := path.Dir(template) + "/temp.docx"
+	_, err := exec.Command(arg0,arg1,arg2,arg3, arg4, arg5 ,dpath).Output()
 	if (err != nil) {
 		log.Fatal(err)
 	} 
-
+	os.Rename(arg5 + "/temp.pdf", outpath)
+	os.Remove(dpath)
 }
 
-// SaveDoc Save doc
-func processDocument(dpath string, data interface{}) {
+func processDocument(tpath string, data interface{}) {
 	fields := reflect.TypeOf(data)
 	values := reflect.ValueOf(data)
 	num := fields.NumField()
 
 	// Read from docx file
-	r, err := docx.ReadDocxFile(dpath)
+	r, err := docx.ReadDocxFile(tpath)
 	if err != nil {
 		panic(err)
 	}
@@ -52,7 +67,8 @@ func processDocument(dpath string, data interface{}) {
 
 	}
 	
-	docx1.WriteToFile(path.Dir(dpath) + "/temp.docx")
+	docx1.WriteToFile(path.Dir(tpath) + "/temp.docx")
 	r.Close()
 	
 }
+
